bf98e5a550f5b8a502c79c4c5c6bdd62ccff8488
Andre-PI <andrelucas_PI@proton.me>
2025-08-27 17:56:48 -0300

    initial commit

---
+++ src/test/java/com/alphasolutions/piauieventos/PiauieventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphasolutions.piauieventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiauieventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
