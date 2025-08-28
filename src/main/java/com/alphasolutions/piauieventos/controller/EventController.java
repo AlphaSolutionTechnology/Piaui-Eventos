30a17a85f4e65a518dcc720831d4a5baddbf170b
Andre-PI <andrelucas_PI@proton.me>
2025-08-27 21:18:49 -0300

    untracked files to tracked fix

---
+++ src/main/java/com/alphasolutions/piauieventos/controller/EventController.java
@@ -0,0 +1,10 @@
+package com.alphasolutions.piauieventos.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+    //TODO
+}
