2f53a06521d859894869a97c51e91439690d9cb9
Antonio Gleyser <antoniogleyser2@hotmail.com>
2025-09-06 18:19:54 -0300

    Feat(Event): Implement EventMapper, EventRepository, and LocationRepository; enhance EventServiceImpl to create events with location mapping.

--- src/main/java/com/alphasolutions/piauieventos/dto/EventResponseDTO.java
+++ src/main/java/com/alphasolutions/piauieventos/dto/EventResponseDTO.java
@@ -1,5 +1,6 @@
 package com.alphasolutions.piauieventos.dto;
 
+import com.alphasolutions.piauieventos.model.Event;
 import java.time.LocalDateTime;
 import lombok.Data;
 
@@ -13,4 +14,16 @@ public class EventResponseDTO {
     String eventType;
     Integer maxSubs;
     Long locationId;
+
+    public EventResponseDTO(Event event) {
+        this.id          = event.getId();
+        this.name        = event.getName();
+        this.description = event.getDescription();
+        this.imageUrl    = event.getImageUrl();
+        this.eventDate   = event.getEventDate();
+        this.eventType   = event.getEventType();
+        this.maxSubs     = event.getMaxSubs();
+        this.locationId  = event.getLocation().getId();
+
+    }
 }
