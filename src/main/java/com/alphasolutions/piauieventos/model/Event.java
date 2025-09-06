10fb759bd47b7c05182cda3e2b6d921194be8db8
Antonio Gleyser <antoniogleyser2@hotmail.com>
2025-09-06 14:50:52 -0300

    Feat(Event): Implementar entidade Event com atributos e mapeamento JPA

--- src/main/java/com/alphasolutions/piauieventos/model/Event.java
+++ src/main/java/com/alphasolutions/piauieventos/model/Event.java
@@ -1,4 +1,39 @@
 package com.alphasolutions.piauieventos.model;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Table(name= "events")
+@Data
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_event")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 150)
+    private String name;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @Column(name = "imagem_url", length = 1000)
+    private String imagemUrl;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDateTime eventDate;
+
+    @Column(name = "event_type", nullable = false, length = 50)
+    private String eventType;
+
+    @Column(name = "max_subs")
+    private Integer maxSubs;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_location", nullable = false)
+    private Location location;
 }
