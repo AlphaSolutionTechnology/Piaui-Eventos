95a3a2b53925f89f8a716a40f61f64498c20d9cb
andrelucas <andrelucas_pi@proton.me>
2025-09-09 10:40:40 -0300

    ignoring .env

--- src/main/java/com/alphasolutions/piauieventos/PiauieventosApplication.java
+++ src/main/java/com/alphasolutions/piauieventos/PiauieventosApplication.java
@@ -1,5 +1,6 @@
 package com.alphasolutions.piauieventos;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PiauieventosApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         SpringApplication.run(PiauieventosApplication.class, args);
     }
 
