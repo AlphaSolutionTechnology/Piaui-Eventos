7842c195845aee710630507c1952668baaeba66c
andrelucas <andrelucas_pi@proton.me>
2025-09-10 08:02:25 -0300

    dotenv

--- src/main/java/com/alphasolutions/piauieventos/PiauieventosApplication.java
+++ src/main/java/com/alphasolutions/piauieventos/PiauieventosApplication.java
@@ -4,12 +4,14 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Objects;
+
 @SpringBootApplication
 public class PiauieventosApplication {
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
         SpringApplication.run(PiauieventosApplication.class, args);
     }
 
