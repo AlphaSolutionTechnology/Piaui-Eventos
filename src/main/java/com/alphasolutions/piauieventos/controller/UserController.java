50e24144749c2c2d33692a1500c672a0558b2b66
Antonio Gleyser <antoniogleyser2@hotmail.com>
2025-09-11 14:40:36 -0300

    Feat(User): Add createUser endpoint to UserController with UserRequestDTO and UserResponseDTO

--- src/main/java/com/alphasolutions/piauieventos/controller/UserController.java
+++ src/main/java/com/alphasolutions/piauieventos/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.alphasolutions.piauieventos.controller;
 
+import com.alphasolutions.piauieventos.dto.UserRequestDTO;
+import com.alphasolutions.piauieventos.dto.UserResponseDTO;
+import com.alphasolutions.piauieventos.service.UserService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO dto) {
+        UserResponseDTO response = userService.createUser(dto);
+
+        return ResponseEntity.ok(response);
+    }
 }
