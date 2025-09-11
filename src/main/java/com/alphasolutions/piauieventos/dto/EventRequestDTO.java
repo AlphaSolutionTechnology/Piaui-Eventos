0df7d37afec7d4af4dc59a8b8455b116d6b76fdf
Antonio Gleyser <antoniogleyser2@hotmail.com>
2025-09-10 22:39:14 -0300

    Refactor(EventDTO): Convert EventRequestDTO and EventResponseDTO to records for improved legibility and immutability

--- src/main/java/com/alphasolutions/piauieventos/dto/EventRequestDTO.java
+++ src/main/java/com/alphasolutions/piauieventos/dto/EventRequestDTO.java
@@ -3,18 +3,13 @@ package com.alphasolutions.piauieventos.dto;
 import java.time.LocalDateTime;
 
 import com.alphasolutions.piauieventos.model.EventLocation;
-import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class EventRequestDTO {
-    String name;
-    String description;
-    String imageUrl;
-    LocalDateTime eventDate;
-    String eventType;
-    Integer maxSubs;
-    EventLocation eventLocation;
-}
+public record EventRequestDTO (
+        String name,
+        String description,
+        String imageUrl,
+        LocalDateTime eventDate,
+        String eventType,
+        Integer maxSubs,
+        EventLocation eventLocation
+) {}
